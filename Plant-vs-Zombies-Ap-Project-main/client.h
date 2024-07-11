985023539efede63ae87f58f7e4c6a77a1020bf9
Ghazala <ghazal1384926054@gmail.com>
2024-07-11 09:45:25 +0430

    get other zombie

--- Plant-vs-Zombies-Ap-Project-main/client.h
+++ Plant-vs-Zombies-Ap-Project-main/client.h
@@ -4,18 +4,13 @@
 #include <QObject>
 #include <QTcpSocket>
 #include "plantzombie.h"
-#include "signup.h"
-#include "login.h"
-#include "forgetpass.h"
+
 class Client : public QObject
 {
     Q_OBJECT
 public:
     explicit Client(plantzombie *game, QObject *parent = nullptr);
     void connectToServer(const QString &address, quint16 port);
-    signup* Signup;
-    login* Login;
-    forgetpass *forget;
 
 public slots:
 
@@ -31,11 +26,6 @@ public slots:
      void sendAddZDMessage(int rect,int x, int y);
      void sendAddZEMessage(int rect,int x, int y);
      void sendAddZFMessage(int rect,int x, int y);
-     void sendSignupMessage(QString message);
-     void sendloginMessage(QString message);
-     void sendforgetpassmessage(QString message);
-     void showuiforget();
-     void showuisignup();
 private slots:
     void onReadyRead();
 
