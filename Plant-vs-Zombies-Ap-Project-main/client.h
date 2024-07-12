fff0cffa95cd1546e78d36df5118c856c1c80302
Ghazala <ghazal1384926054@gmail.com>
2024-07-12 05:58:08 +0430

    endddd

--- Plant-vs-Zombies-Ap-Project-main/client.h
+++ Plant-vs-Zombies-Ap-Project-main/client.h
@@ -4,31 +4,14 @@
 #include <QObject>
 #include <QTcpSocket>
 #include "plantzombie.h"
-#include "signup.h"
-#include "login.h"
-#include "forgetpass.h"
-#include "voroodi2.h"
-#include "historygameshowww.h"
-#include <QJsonArray>
+
 class Client : public QObject
 {
     Q_OBJECT
 public:
     explicit Client(plantzombie *game, QObject *parent = nullptr);
     void connectToServer(const QString &address, quint16 port);
-    void EndTime(QString message);
-    void EndTime2(QString message);
-    signup* Signup;
-    login* Login;
-    forgetpass *forget;
-    voroodi2 *vorood;
-    historyGameshowww *HGS;
-    //int v;
-    QString ZOrP;
-    QString number;
-    bool checkv=false;
-    QTimer *Timercheckvorod;
-    bool checknumber=false;
+
 public slots:
 
      void sendAddPAMessage(int rect,int x, int y);
@@ -43,39 +26,12 @@ public slots:
      void sendAddZDMessage(int rect,int x, int y);
      void sendAddZEMessage(int rect,int x, int y);
      void sendAddZFMessage(int rect,int x, int y);
-
-     void sendAddPAMessage2(int rect,int x, int y);
-     void sendAddPBMessage2(int rect,int x, int y);
-     void sendAddPCMessage2(int rect,int x, int y);
-     void sendAddPDMessage2(int rect,int x, int y);
-     void sendAddPEMessage2(int rect,int x, int y);
-     void sendAddPFMessage2(int rect,int x, int y);
-     void sendAddZAMessage2(int rect,int x, int y);
-     void sendAddZBMessage2(int rect,int x, int y);
-     void sendAddZCMessage2(int rect,int x, int y);
-     void sendAddZDMessage2(int rect,int x, int y);
-     void sendAddZEMessage2(int rect,int x, int y);
-     void sendAddZFMessage2(int rect,int x, int y);
-
-     void sendSignupMessage(QString message);
-     void sendloginMessage(QString message);
-     void sendforgetpassmessage(QString message);
-     void showuiforget();
-     void showuisignup();
-     void EndMap(QString message);
-     //void EndTime(QString message);
-     void EndMap2(QString message);
-     //void EndTime2(QString message);
-     void startforgame();
-     void checkstartvorood();
-     void historyShow();
 private slots:
     void onReadyRead();
 
 private:
     QTcpSocket *m_socket;
     plantzombie *m_game;
-    plantzombie *m_game2;
 };
 
 #endif // CLIENT_H
